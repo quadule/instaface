4b771376420f63828ce84d9a8e580d0829317155
Kyle Meyer <kaiuhl@gmail.com>
2012-03-02 22:35:37 -0800

    Remove instagram from the controller because that was super dumb. sorry guys. :[

--- app/models/instagram_search.rb
+++ app/models/instagram_search.rb
@@ -1,5 +1,12 @@
 class InstagramSearch
-	def self.at(lat, lng)
+	def initialize
+		Instagram.configure do |config|
+			config.client_id = INSTAGRAM_CLIENT_ID
+			config.client_secret = INSTAGRAM_CLIENT_SECRET
+		end
+	end
+	
+	def at(lat, lng)
 		Instagram.media_search(lat, lng)
 	end
 end
