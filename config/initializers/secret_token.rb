bb82244d936f69941852481f2bcefea5d770b4cb
Milo Winningham <milo@winningham.net>
2012-03-02 21:56:14 -0800

    base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instaface::Application.config.secret_token = '0a27aa1cda560e1682585c4e8cf956101cb325638ab8c472d588efa2f0de89ed3063153c17852a57d98f0ea2a8fe6826ed4f1a294141b32d7ed156b6c14d1256'
