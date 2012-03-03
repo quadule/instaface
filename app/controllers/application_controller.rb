44a2a52c4586301c21c3cb2d00a2b9668a9f02b6
Kyle Meyer <kaiuhl@gmail.com>
2012-03-02 22:25:29 -0800

    Instagram API integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	before_filter :configure_instagram
+
+	private
+	
+	def configure_instagram
+		Instagram.configure do |config|
+			config.client_id = INSTAGRAM_CLIENT_ID
+			config.client_secret = INSTAGRAM_CLIENT_SECRET
+		end
+	end
 end
---
+++ spec/models/instagram_search_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe InstagramSearch do
+  let!(:instagram) { InstagramSearch.faces_at(45.5, -122.69) }
+
+	it "returns a hash including photos" do
+		instagram.class.should eq(Hash)
+	end
+end
