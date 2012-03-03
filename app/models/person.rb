d599f5bf39477fdc26015d888db763c3ade78f6f
Milo Winningham <milo@winningham.net>
2012-03-03 00:08:21 -0800

    build people from face information in an image

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,4 +5,10 @@ class Person < ActiveRecord::Base
     @@api ||= Face.get_client api_key:    ENV.fetch('FACE_API_KEY'),
                               api_secret: ENV.fetch('FACE_API_SECRET')
   end
+  
+  def self.create_from_faces(image_url)
+    response = api.faces_detect(:urls => image_url, :attributes => :all)
+    faces = response['photos'].first['tags']
+    faces.map { |face| create(:data => face) }
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -5,4 +5,13 @@ describe Person do
     subject { Person.api }
     it { should respond_to(:faces_detect) }
   end
+  
+  describe '.create_from_faces' do
+    context 'using an image of two people' do
+      image_url = "http://www.comicmix.com/wp-content/uploads/2011/02/prince-william-kate-middleton.jpg"
+      subject { Person.create_from_faces(image_url) }
+      
+      it { should have(2).people }
+    end
+  end
 end
